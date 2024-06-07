d43560d0216aa58f01fe93cadfc45a2fec6ecfea
Thanh Tho <letanhtho1953@gmail.com>
2024-06-07 14:00:18 +0700

    cap nhat order 1 san pham trong gio hang

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -7,7 +7,7 @@ const User = require("../models/User");
 
 class OrderController {
   async createOrder(req, res) {
-    const { userId, orderFromCart, orderDetails } = req.body;
+    const { userId, orderFromCart, orderDetails, singleCartItem } = req.body;
     try {
       const user = await User.findById(userId);
       if (!user) {
@@ -15,14 +15,24 @@ class OrderController {
       }
       const { email } = user;
       if (orderFromCart) {
-        // Tìm giỏ hàng của người dùng
         const userCart = await Cart.findOne({ user_id: userId });
         if (!userCart) {
           return res.status(404).json({ message: "Cart not found" });
         }
-        // Kiểm tra số lượng sản phẩm trong giỏ hàng và số lượng sản phẩm có sẵn trong kho
-        for (const item of userCart.items) {
-          const { product_id, quantity } = item;
+
+        if (singleCartItem) {
+          const { product_id } = singleCartItem;
+          const item = userCart.items.find(
+            (item) => item.product_id == product_id
+          );
+
+          if (!item) {
+            return res
+              .status(404)
+              .json({ message: "Product not found in cart" });
+          }
+
+          const quantity = item.quantity;
           const product = await Product.findById(product_id);
           if (!product) {
             return res.status(404).json({ message: "Product not found" });
@@ -32,92 +42,121 @@ class OrderController {
               .status(400)
               .json({ message: "Not enough quantity for product" });
           }
-        }
-        // Tạo đơn hàng mới
-        const newOrder = new Order({
-          user_id: userId,
-          status: "Pending", // Ví dụ: trạng thái ban đầu là "Pending"
-        });
-        // Lưu đơn hàng mới
-        const savedOrder = await newOrder.save();
-        let totalPrice = 0;
-        // Lưu chi tiết đơn hàng và xóa sản phẩm từ giỏ hàng
-        for (const item of userCart.items) {
-          const { product_id, quantity } = item;
-          // Tìm thông tin sản phẩm
-          const product = await Product.findById(product_id);
-          // Tính tổng giá trị của sản phẩm
+
+          const newOrder = new Order({
+            user_id: userId,
+            status: "Pending",
+          });
+          const savedOrder = await newOrder.save();
+
           const productTotalPrice = product.price * quantity;
-          totalPrice += productTotalPrice;
-          // Tạo chi tiết đơn hàng từ sản phẩm trong giỏ hàng
           const newOrderDetail = new OrderDetail({
             order_id: savedOrder._id,
             product_id,
             quantity,
-            unit_price: product.price, // Lưu giá của sản phẩm vào chi tiết đơn hàng
+            unit_price: product.price,
           });
-          // Lưu chi tiết đơn hàng
           await newOrderDetail.save();
-          // Cập nhật số lượng sản phẩm trong kho
+
           product.quantity -= quantity;
           await product.save();
-          // Xóa sản phẩm đã đặt hàng khỏi giỏ hàng
+
           await Cart.updateOne(
             { _id: userCart._id },
             { $pull: { items: { product_id } } }
           );
+
+          savedOrder.total_price = productTotalPrice;
+          await savedOrder.save();
+
+          const orderController = new OrderController();
+          await orderController.sendConfirmationEmail(email);
+
+          return res
+            .status(200)
+            .json({ message: "Order successfully", data: savedOrder });
+        } else {
+          for (const item of userCart.items) {
+            const { product_id, quantity } = item;
+            const product = await Product.findById(product_id);
+            if (!product) {
+              return res.status(404).json({ message: "Product not found" });
+            }
+            if (quantity > product.quantity) {
+              return res
+                .status(400)
+                .json({ message: "Not enough quantity for product" });
+            }
+          }
+
+          const newOrder = new Order({
+            user_id: userId,
+            status: "Pending",
+          });
+          const savedOrder = await newOrder.save();
+          let totalPrice = 0;
+
+          for (const item of userCart.items) {
+            const { product_id, quantity } = item;
+            const product = await Product.findById(product_id);
+            const productTotalPrice = product.price * quantity;
+            totalPrice += productTotalPrice;
+
+            const newOrderDetail = new OrderDetail({
+              order_id: savedOrder._id,
+              product_id,
+              quantity,
+              unit_price: product.price,
+            });
+            await newOrderDetail.save();
+
+            product.quantity -= quantity;
+            await product.save();
+
+            await Cart.updateOne(
+              { _id: userCart._id },
+              { $pull: { items: { product_id } } }
+            );
+          }
+          savedOrder.total_price = totalPrice;
+          await savedOrder.save();
+
+          const orderController = new OrderController();
+          await orderController.sendConfirmationEmail(email);
+
+          return res
+            .status(200)
+            .json({ message: "Order successfully", data: savedOrder });
         }
-        // Cập nhật tổng giá vào đơn hàng
-        savedOrder.total_price = totalPrice;
-        await savedOrder.save();
-        const orderController = new OrderController();
-        await orderController.sendConfirmationEmail(email);
-        return res
-          .status(200)
-          .json({ message: "Order successfully", data: savedOrder });
       } else {
-        // Order trực tiếp sản phẩm
-        let totalPrice = 0;
-        // Lưu đơn hàng mới
+        const { product_id, quantity, unit_price } = orderDetails;
+        const product = await Product.findById(product_id);
+
+        if (!product) {
+          return res.status(404).json({ message: "Product not found" });
+        }
+        if (quantity > product.quantity) {
+          return res
+            .status(400)
+            .json({ message: "Not enough quantity for product" });
+        }
+
         const newOrder = new Order({
           user_id: userId,
-          status: "Pending", // Ví dụ: trạng thái ban đầu là "Pending"
+          status: "Pending",
         });
         const savedOrder = await newOrder.save();
-        // Kiểm tra số lượng sản phẩm trong kho trước khi đặt hàng
-        for (const detail of orderDetails) {
-          const { product_id, quantity } = detail;
-          const product = await Product.findById(product_id);
-
-          if (!product) {
-            return res.status(404).json({ message: "Product not found" });
-          }
-          if (quantity > product.quantity) {
-            return res
-              .status(400)
-              .json({ message: "Not enough quantity for product" });
-          }
-          // Tính tổng giá trị của sản phẩm
-          const productTotalPrice = product.price * quantity;
-          totalPrice += productTotalPrice;
-        }
-        // Lưu chi tiết đơn hàng
-        for (const detail of orderDetails) {
-          const { product_id, quantity, unit_price } = detail;
-          const newOrderDetail = new OrderDetail({
-            order_id: savedOrder._id,
-            product_id,
-            quantity,
-            unit_price,
-          });
-          await newOrderDetail.save();
-          // Cập nhật số lượng sản phẩm trong kho
-          const product = await Product.findById(product_id);
-          product.quantity -= quantity;
-          await product.save();
-        }
-        // Cập nhật tổng giá vào đơn hàng
-        savedOrder.total_price = totalPrice;
+        const productTotalPrice = product.price * quantity;
+        const newOrderDetail = new OrderDetail({
+          order_id: savedOrder._id,
+          product_id,
+          quantity,
+          unit_price,
+        });
+        await newOrderDetail.save();
+        product.quantity -= quantity;
+        await product.save();
+        savedOrder.total_price = productTotalPrice;
         await savedOrder.save();
         const orderController = new OrderController();
         await orderController.sendConfirmationEmail(email);
